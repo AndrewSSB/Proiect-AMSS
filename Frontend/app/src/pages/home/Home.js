c32c51b9e49bf21fd3a91d56168669204366aa32
GemanAmalia <amalia_alexa00@yahoo.com>
2024-01-15 22:14:51 +0200

    update home page and add navigation bar

--- Frontend/app/src/pages/home/Home.js
+++ Frontend/app/src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Navbar from "../navbar/navbar";
 import {ReactComponent as Reading} from './reading.svg';
 import { useState } from "react";
 import { _get } from "../../utils/api";
@@ -18,14 +19,42 @@ const Home = () => {
       deleteToken();
       history("/");
     }
-    return (
-        <div>
-            <p> home page</p>
-            <div >
-              <button id="logout-button" onClick={handleLogOut}>Logout</button>
-            </div>
-        </div>
-    )
+
+
+    if (token && (token?.role === "User" || token?.role === "Admin")) {
+        return(
+            <>
+            <Navbar></Navbar>
+            <body>
+                <header className="header-container">
+                    <h1> Book Hub</h1>
+                    <p>Connecting book lovers through the joy of sharing and swapping stories.</p>
+                </header>
+
+                <div className="body-container">
+                    <h2>Welcome to Your Book Exchange!</h2>
+                    <p>Discover a community of avid readers who love to share their favorite books. Whether you're looking to
+                        explore new genres or share your well-loved classics, our book exchange platform brings readers together.
+                        Join us and embark on a literary journey like never before.</p>
+
+                    <h2>How it Works</h2>
+                    <p>
+                        1. Browse available books for exchange<br></br>
+                        2. Request a swap with other user<br></br>
+                        3. Connect with fellow book enthusiasts<br></br>
+                        4. Share your thoughts and experiences
+                    </p>
+
+                    <h2>Start Exchanging Today!</h2>
+                    <p>Join our growing community of book lovers and experience the joy of sharing stories.</p>
+                </div>
+            </body>
+            </>
+           
+        )
+    } else {
+        history("/Authentication/Login");
+    }
     
 };
 
